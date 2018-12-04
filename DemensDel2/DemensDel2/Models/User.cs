80ea4f53dc517934676f036169f393487596602a
Steffen Kiilsholm Hansen <stef7182@edu.eal.dk>
2018-12-04 10:13:48 +0100

    Dbcontext class created

---
+++ DemensDel2/DemensDel2/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemensDel2.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        //Reference to User Identity data
+        public string UserIdentityID { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int TelephoneNumber { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public Log Log { get; set; }
+    }
+}
---
+++ DemensDel2/DemensDel2API/Models/DemensDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DemensDel2.Models;
+
+namespace DemensDel2API.Models
+{
+    public class DemensDbContext : DbContext
+    {
+        public DemensDbContext(DbContextOptions<DemensDbContext> options) : base(options) 
+        {
+            //Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
