50130dd9f2772703e848ac27a247f95e43b80544
Jesper <jesp626a@edu.eal.dk>
2018-12-04 10:14:29 +0100

    Added Exercie, ExerciseType, Log and TrainingSession classes

---
+++ DemensDel2/DemensDel2/Models/ExerciseType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemensDel2.Models
+{
+    public class ExerciseType
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+        public float Duration { get; set; }
+        public string Description { get; set; }
+        public string MuscleGroup { get; set; }
+        public int Difficulty { get; set; }
+    }
+}
