8e8779fb09c0718579b86e3df32b681c601a1ffa
Jesper <jesp626a@edu.eal.dk>
2018-12-04 10:58:25 +0100

    Added migrations

--- DemensDel2/DemensDel2API/Startup.cs
+++ DemensDel2/DemensDel2API/Startup.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DemensDel2API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -11,8 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
-using DemensDel2API.Models;
+using DemensDel2API.DataAccess;
 
 namespace DemensDel2API
 {
@@ -31,20 +29,14 @@ namespace DemensDel2API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<DemensDbContext>(options =>
-            //{
-            //    var connectionString = configuration.GetConnectionString("DemensDbContext");
-            //    options.UseSqlServer(connectionString);
-            //});
+            services.AddDbContext<DemensDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("DemensDbContext");
+                options.UseSqlServer(connectionString);
+            });
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            services.AddDbContext<DemensDbContext>(options =>
-            {
-                var connectionString = configuration.GetConnectionString("UserDbContext");
-                options.UseSqlServer(connectionString);
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
