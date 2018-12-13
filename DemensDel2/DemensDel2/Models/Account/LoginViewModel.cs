f8582d1eba70beaddf3dc09a14722a32cf72df2e
Emil Yde Hagelsø <emil748c@edu.eal.dk>
2018-12-13 11:28:45 +0100

    Noget identity som ikke gider at virke

---
+++ DemensDel2/DemensDel2/Models/Account/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemensDel2.Models.Account
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress, MaxLength(300)]
+        [Display(Name = "Email Addresse")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ DemensDel2/DemensDel2API/DataAccess/DemensLoginDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemensDel2API.DataAccess
+{
+    public class DemensLoginDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DemensLoginDbContext(DbContextOptions<DemensLoginDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
