e0d7a7b454db67ae671a4b577aca904d5160510c
Jesper <jesp626a@edu.eal.dk>
2018-12-13 14:44:29 +0100

    Pre-Database rework

---
+++ DemensDel2/DemensDel2/Models/UserTrainingSessionRelation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemensDel2.Models
+{
+    public class UserTrainingSessionRelation
+    {
+        public virtual User Users { get; set; }
+        public virtual TrainingSession TrainingSessions { get; set; }
+        public virtual double Percent { get; set; }
+        
+
+    }
+}
